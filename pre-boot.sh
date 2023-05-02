9b2552ada97fef591bc1b8592da3a406aaef5058
Joel Hooks <joel@egghead.io>
2023-05-01 17:01:07 -0700

    update again

--- pre-boot.sh
+++ pre-boot.sh
@@ -18,8 +18,7 @@ sudo apt update \
  && sudo apt autoremove -y
 
 # Install latest CUDA and NVIDIA drivers
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
-sudo apt update && sudo apt -y install cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
